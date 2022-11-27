360edc2b29d531cfa6bac423b5ff2792c6c4b811
havrintuula <90961099+havrintuula@users.noreply.github.com>
2022-11-27 20:42:52 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN = '5714353815:AAE-IeAng2zSjxGTkBZVzWXOaQ5NslfZTLQ'
+exchanges = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+    'юань': 'CNY',
+}
---
+++ extensions.py
@@ -0,0 +1,36 @@
+import requests
+import json
+from config import exchanges
+
+
+class APIException(Exception):
+    pass
+
+
+class Convert:
+    @staticmethod
+    def get_price(quote: str, base: str, amount: str):
+        if quote == base:
+            raise APIException(f'Невозможно конвертировать одинаковые валюты {base}.')
+
+        try:
+            quote_ticker = exchanges[quote]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {quote}')
+
+        try:
+            base_ticker = exchanges[base]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {base}')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f'Не удалось обработать количество {amount}')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = json.loads(r.content)[exchanges[base]]
+
+        return total_base
+
+
